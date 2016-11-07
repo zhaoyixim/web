3c2c463a6cdb181a94dab92a4cbb37770bf3396c
zhaoyixim <zhao.hey@foxmail.com>
2016-11-07 17:17:26 +0800

    add first

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -6,48 +6,300 @@ $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+    <!-- Section: intro -->
+    <section id="intro" class="intro">
+        <div class="slogan">
+            <h1>This is Ninestars</h1>
+            <p>make awesome stuff with Bootstrap framework</p>
+            <a href="#about" class="btn btn-skin scroll">Learn more</a>
+        </div>
+    </section>
+    <!-- /Section: intro -->
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+    <!-- Section: about -->
+    <section id="about" class="home-section text-center bg-gray">
+        <div class="heading-about marginbot-50">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
+                        <div class="section-heading">
+                            <h2>About us</h2>
+                            <p>Lorem ipsum dolor sit amet, no nisl mentitum recusabo per, vim at blandit qualisque dissentiunt. Diam efficiantur conclusionemque ut has</p>
+                        </div>
 
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                    </div>
+                </div>
             </div>
         </div>
+        <div class="container">
 
-    </div>
+            <div class="row">
+                <div class="col-xs-6 col-sm-3 col-md-3">
+
+                    <div class="team boxed-grey">
+                        <div class="inner">
+                            <h5>Anna Hanaceck</h5>
+                            <p class="subtitle">Pixel Crafter</p>
+                            <div class="avatar"><img src="img/team/1.jpg" alt="" class="img-responsive" /></div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-xs-6 col-sm-3 col-md-3">
+
+                    <div class="team boxed-grey">
+                        <div class="inner">
+                            <h5>Maura Daniels</h5>
+                            <p class="subtitle">Ruby on Rails</p>
+                            <div class="avatar"><img src="img/team/2.jpg" alt="" class="img-responsive" /></div>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-xs-6 col-sm-3 col-md-3">
+
+                    <div class="team boxed-grey">
+                        <div class="inner">
+                            <h5>Jack Briane</h5>
+                            <p class="subtitle">jQuery Ninja</p>
+                            <div class="avatar"><img src="img/team/3.jpg" alt="" class="img-responsive" /></div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-xs-6 col-sm-3 col-md-3">
+
+                    <div class="team boxed-grey">
+                        <div class="inner">
+                            <h5>Tom Petterson</h5>
+                            <p class="subtitle">Typographer</p>
+                            <div class="avatar"><img src="img/team/4.jpg" alt="" class="img-responsive" /></div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- /Section: about -->
+
+
+    <!-- Section: services -->
+    <section id="service" class="home-section text-center">
+
+        <div class="heading-about marginbot-50">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+
+                        <div class="section-heading">
+                            <h2>Our Services</h2>
+                            <p>Lorem ipsum dolor sit amet, no nisl mentitum recusabo per, vim at blandit qualisque dissentiunt. Diam efficiantur conclusionemque ut has</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3 col-md-3">
+
+                    <div class="service-box">
+                        <div class="service-icon">
+                            <i class="fa fa-code fa-3x"></i>
+                        </div>
+                        <div class="service-desc">
+                            <h5>Print</h5>
+                            <p>Vestibulum tincidunt enim in pharetra malesuada. Duis semper magna metus electram accommodare.</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-sm-3 col-md-3">
+
+                    <div class="service-box">
+                        <div class="service-icon">
+                            <i class="fa fa-suitcase fa-3x"></i>
+                        </div>
+                        <div class="service-desc">
+                            <h5>Web Design</h5>
+                            <p>Vestibulum tincidunt enim in pharetra malesuada. Duis semper magna metus electram accommodare.</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-sm-3 col-md-3">
+
+                    <div class="service-box">
+                        <div class="service-icon">
+                            <i class="fa fa-cog fa-3x"></i>
+                        </div>
+                        <div class="service-desc">
+                            <h5>Photography</h5>
+                            <p>Vestibulum tincidunt enim in pharetra malesuada. Duis semper magna metus electram accommodare.</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-sm-3 col-md-3">
+
+                    <div class="service-box">
+                        <div class="service-icon">
+                            <i class="fa fa-rocket fa-3x"></i>
+                        </div>
+                        <div class="service-desc">
+                            <h5>Cloud System</h5>
+                            <p>Vestibulum tincidunt enim in pharetra malesuada. Duis semper magna metus electram accommodare.</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- /Section: services -->
+
+
+    <!-- Section: works -->
+    <section id="works" class="home-section text-center bg-gray">
+        <div class="heading-works marginbot-50">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+
+                        <div class="section-heading">
+                            <h2>Recent Works</h2>
+                            <p>Lorem ipsum dolor sit amet, no nisl mentitum recusabo per, vim at blandit qualisque dissentiunt. Diam efficiantur conclusionemque ut has</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-12" >
+
+                    <div class="row gallery-item">
+                        <div class="col-md-3">
+                            <a href="img/works/1.jpg" title="This is an image title" data-lightbox-gallery="gallery1" data-lightbox-hidpi="img/works/1@2x.jpg">
+                                <img src="img/works/1.jpg" class="img-responsive" alt="img">
+                            </a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="img/works/2.jpg" title="This is an image title" data-lightbox-gallery="gallery1" data-lightbox-hidpi="img/works/1@2x.jpg">
+                                <img src="img/works/2.jpg" class="img-responsive" alt="img">
+                            </a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="img/works/3.jpg" title="This is an image title" data-lightbox-gallery="gallery1" data-lightbox-hidpi="img/works/1@2x.jpg">
+                                <img src="img/works/3.jpg" class="img-responsive" alt="img">
+                            </a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="img/works/4.jpg" title="This is an image title" data-lightbox-gallery="gallery1" data-lightbox-hidpi="img/works/1@2x.jpg">
+                                <img src="img/works/4.jpg" class="img-responsive" alt="img">
+                            </a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="img/works/5.jpg" title="This is an image title" data-lightbox-gallery="gallery1" data-lightbox-hidpi="img/works/1@2x.jpg">
+                                <img src="img/works/5.jpg" class="img-responsive" alt="img">
+                            </a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="img/works/6.jpg" title="This is an image title" data-lightbox-gallery="gallery1" data-lightbox-hidpi="img/works/1@2x.jpg">
+                                <img src="img/works/6.jpg" class="img-responsive" alt="img">
+                            </a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="img/works/7.jpg" title="This is an image title" data-lightbox-gallery="gallery1" data-lightbox-hidpi="img/works/1@2x.jpg">
+                                <img src="img/works/7.jpg" class="img-responsive" alt="img">
+                            </a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="img/works/8.jpg" title="This is an image title" data-lightbox-gallery="gallery1" data-lightbox-hidpi="img/works/1@2x.jpg">
+                                <img src="img/works/8.jpg" class="img-responsive" alt="img">
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- /Section: works -->
+
+    <!-- Section: contact -->
+    <section id="contact" class="home-section text-center">
+        <div class="heading-contact marginbot-50">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+
+                        <div class="section-heading">
+                            <h2>Get in touch</h2>
+                            <p>Lorem ipsum dolor sit amet, no nisl mentitum recusabo per, vim at blandit qualisque dissentiunt. Diam efficiantur conclusionemque ut has</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-8 col-md-offset-2">
+                    <div class="boxed-grey">
+                        <div id="sendmessage">Your message has been sent. Thank you!</div>
+                        <div id="errormessage"></div>
+                        <form action="" method="post" role="form" class="contactForm">
+                            <div class="form-group">
+                                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                <div class="validation"></div>
+                            </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                                <div class="validation"></div>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                                <div class="validation"></div>
+                            </div>
+                            <div class="form-group">
+                                <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                                <div class="validation"></div>
+                            </div>
+
+                            <div class="text-center"><button type="submit" class="btn btn-skin">Send Message</button></div>
+                        </form>
+                    </div>
+
+                    <div class="widget-contact row">
+                        <div class="col-lg-6">
+                            <address>
+                                <strong>Ninestars Ltd.</strong><br>
+                                Big Villa 334 Awesome, Beautiful Suite 1200<br>
+                                San Francisco, CA 94107<br>
+                                <abbr title="Phone">P:</abbr> (123) 456-7890
+                            </address>
+                        </div>
+
+                        <div class="col-lg-6">
+                            <address>
+                                <strong>Email</strong><br>
+                                <a href="mailto:#">email.name@example.com</a><br />
+                                <a href="mailto:#">name.name@example.com</a>
+                            </address>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
 </div>
