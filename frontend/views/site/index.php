51580cc9aae3f5be5530a6d1d58ee9952b333fe0
zhaoyixim <zhao.hey@foxmail.com>
2016-11-09 15:13:32 +0800

    index page 2016-11-09

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -8,7 +8,7 @@ $this->title = 'hongjinwenhua';
 
 
 $this->registerCssFile('@web/css/style.css');
-$this->registerCssFile('@web/css/lazyload/bootstrapTheme.css');
+//$this->registerCssFile('@web/css/lazyload/bootstrapTheme.css');
 
 $this->registerCssFile('@web/css/lazyload/owl.carousel.css');
 $this->registerCssFile('@web/css/lazyload/owl.theme.css');
@@ -21,7 +21,7 @@ $this->registerJsFile('@web/js/lazyload/owl.carousel.js', ['depends' => ['fronte
 
     <!-- Section: intro -->
     <section id="intro" class="intro">
-        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <div id="carousel-generic" class="carousel slide" data-ride="carousel">
             <!-- Indicators -->
             <ol class="carousel-indicators">
                 <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
@@ -63,29 +63,119 @@ $this->registerJsFile('@web/js/lazyload/owl.carousel.js', ['depends' => ['fronte
         </div>
     </section>
     <!-- /Section: intro -->
-    <div class="container b1">
+    <div class="container">
 
         <div class="row">
-            <div class="span12">
-                            <div id="owl-demo" class="owl-carousel">
-                                <div class="item"><img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl1.jpg',true)?>" alt="Lazy Owl Image"></div>
-                                <div class="item"><img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl2.jpg',true)?>" alt="Lazy Owl Image"></div>
-                                <div class="item"><img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl3.jpg',true)?>" alt="Lazy Owl Image"></div>
-                                <div class="item"><img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl4.jpg',true)?>" alt="Lazy Owl Image"></div>
-                                <div class="item"><img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl1.jpg',true)?>" alt="Lazy Owl Image"></div>
-                                <div class="item"><img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl5.jpg',true)?>" alt="Lazy Owl Image"></div>
-                                <div class="item"><img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl7.jpg',true)?>" alt="Lazy Owl Image"></div>
-                                <div class="item"><img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl8.jpg',true)?>" alt="Lazy Owl Image"></div>
-                                <div class="item"><img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl1.jpg',true)?>" alt="Lazy Owl Image"></div>
-                                <div class="item"><img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl2.jpg',true)?>" alt="Lazy Owl Image"></div>
-                                <div class="item"><img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl3.jpg',true)?>" alt="Lazy Owl Image"></div>
-                                <div class="item"><img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl1.jpg',true)?>" alt="Lazy Owl Image"></div>
-                                <div class="item"><img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl4.jpg',true)?>" alt="Lazy Owl Image"></div>
-                                <div class="item"><img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl6.jpg',true)?>" alt="Lazy Owl Image"></div>
-                                <div class="item"><img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl7.jpg',true)?>" alt="Lazy Owl Image"></div>
-                                <div class="item"><img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl8.jpg',true)?>" alt="Lazy Owl Image"></div>
+            <div class="col-xs-12">
+                <div id="owl-demo" class="owl-carousel margin-top20">
+                    <div class="item">
+                        <a href="#">
+                            <div class="owl-label">
+                                <p class="text-center color-white">abdifb</pc>
                             </div>
+                            <img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl1.jpg',true)?>" alt="Lazy Owl Image">
+                        </a>
+                    </div>
+
+                    <div class="item">
+                        <a href="#">
+                            <div class="owl-label">
+                                <p class="text-center color-white">abdifb</pc>
+                            </div>
+                            <img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl1.jpg',true)?>" alt="Lazy Owl Image">
+                        </a>
+                    </div>
+
+                    <div class="item">
+                        <a href="#">
+                            <div class="owl-label">
+                                <p class="text-center color-white">abdifb</pc>
+                            </div>
+                            <img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl2.jpg',true)?>" alt="Lazy Owl Image">
+                        </a>
+                    </div>
+
+                    <div class="item">
+                        <a href="#">
+                            <div class="owl-label">
+                                <p class="text-center color-white">abdifb</pc>
+                            </div>
+                            <img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl3.jpg',true)?>" alt="Lazy Owl Image">
+                        </a>
+                    </div>
+
+                    <div class="item">
+                        <a href="#">
+                            <div class="owl-label">
+                                <p class="text-center color-white">abdifb</pc>
+                            </div>
+                            <img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl4.jpg',true)?>" alt="Lazy Owl Image">
+                        </a>
+                    </div>
+
+                    <div class="item">
+                        <a href="#">
+                            <div class="owl-label">
+                                <p class="text-center color-white">abdifb</pc>
+                            </div>
+                            <img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl5.jpg',true)?>" alt="Lazy Owl Image">
+                        </a>
+                    </div>
+                    <div class="item">
+                        <a href="#">
+                            <div class="owl-label">
+                                <p class="text-center color-white">abdifb</pc>
+                            </div>
+                            <img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl6.jpg',true)?>" alt="Lazy Owl Image">
+                        </a>
+                    </div>
+                    <div class="item">
+                        <a href="#">
+                            <div class="owl-label">
+                                <p class="text-center color-white">abdifb</pc>
+                            </div>
+                            <img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl7.jpg',true)?>" alt="Lazy Owl Image">
+                        </a>
+                    </div>
+
+                    <div class="item">
+                        <a href="#">
+                            <div class="owl-label">
+                                <p class="text-center color-white">abdifb</pc>
+                            </div>
+                            <img class="lazyOwl" data-src="<?=Url::to('@web/images/lazyload/owl8.jpg',true)?>" alt="Lazy Owl Image">
+                        </a>
+                    </div>
+
+
+                </div>
             </div>
+        </div>
+
+        <div class="row block-text">
+            <div class="col-xs-12 block-heigh b1">
+
+                <div class="col-xs-4 b1">
+
+                    <div class="block-left b0">
+                        img
+
+                    </div>
+                    <div class="block-right"></div>
+
+
+                </div>
+                <div class="col-xs-8 b1">
+
+                    <div class="block-left"></div>
+
+
+                </div>
+
+
+            </div>
+
+            <div class="clearfix"></div>
         </div>
 
 
@@ -103,9 +193,18 @@ $(document).ready(function() {
   $("#owl-demo").owlCarousel({
     items : 4,
     lazyLoad : true,
-    navigation : true
+    navigation : false,
+    pagination : false
   });
 
+    $('#carousel-generic').carousel({
+        interval: 3000,
+    });
+
+    $(".item").hover(function(){
+        $(this).find('.owl-label').toggle();
+
+    });
 });
 <?php $this->endBlock() ?>
 <?php $this->registerJs($this->blocks['lazyload'], \yii\web\View::POS_END); ?>
